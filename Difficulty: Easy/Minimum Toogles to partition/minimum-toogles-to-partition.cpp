a520b16086ba50ce0a0464f0cf758692a26c7779
Yash Gangwar <103266801+YashGangwar3956@users.noreply.github.com>
2026-05-26 00:16:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Minimum Toogles to partition/minimum-toogles-to-partition.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minToggle(vector<int>& arr) {
+
+        int totalZero = 0;
+
+        for (int x : arr)
+            if (x == 0)
+                totalZero++;
+
+        int leftOnes = 0;
+        int rightZeros = totalZero;
+
+        int ans = rightZeros;
+
+        for (int x : arr) {
+
+            if (x == 1)
+                leftOnes++;
+
+            else
+                rightZeros--;
+
+            ans = min(ans, leftOnes + rightZeros);
+        }
+
+        return ans;
+    }
+};
